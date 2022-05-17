12989718c67b3d142648f014e436b7bc3000c421
Broadbridge <broadbridgek@aetna.com>
2022-05-17 18:11:44 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './UI/Header';
+import Main from './UI/Main';
+import Deployments from './Deployments/Deployments';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header></Header>
+    <Main>
+      <Deployments></Deployments>
+    </Main>
+    </>
   );
 }
 
